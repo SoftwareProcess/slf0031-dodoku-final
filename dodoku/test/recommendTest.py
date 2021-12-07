b0dfc2e851cf1ec3d2e959ec6a4053eff2daa537
slf0031 <91715755+slf0031@users.noreply.github.com>
2021-12-06 23:37:30 -0600

    Update recommendTest.py

--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -1,5 +1,74 @@
 from unittest import TestCase
-import dodoku.status as recommend 
+import dodoku.recommend as recommend 
+from dodoku import recommend
+
 
 class RecommendTest(TestCase):
-        pass
+    #Happy Path Test Cases
+     def test100_101ShouldReturnValidRecommendation(self):
+         parms={'cell':'r1c1','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = {'recommendation': [3, 6, 7, 9], 'status': 'ok'}
+         actualResult = recommend._recommend(parms)
+                
+         self.assertEqual(expectedResult, actualResult)
+         
+     def test100_102ShouldReturnValidRec2(self):
+         parms={'cell':'r1c3','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = {'recommendation': [3, 5, 7, 8], 'status': 'ok'}
+         actualResult = recommend._recommend(parms)
+                  
+         self.assertEqual(expectedResult, actualResult)
+        
+     def test100_103ShouldReturnValidRec3(self):
+         parms={'cell':'r3c3','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = {'recommendation': [2, 4, 5, 7, 8], 'status': 'ok'}
+         actualResult = recommend._recommend(parms)
+                  
+         self.assertEqual(expectedResult, actualResult)    
+     def test100_104ShouldReturnValidRec4(self):
+         parms={'cell':'r12c13','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = {'recommendation': [2, 3, 5, 6, 8], 'status': 'ok'}
+         actualResult = recommend._recommend(parms)
+                  
+         self.assertEqual(expectedResult, actualResult)
+     def test100_105ShouldReturnValidRec5(self):
+         parms={'cell':'r8c8','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = {'recommendation': [7], 'status': 'ok'}
+         actualResult = recommend._recommend(parms)
+                  
+         self.assertEqual(expectedResult, actualResult)
+         
+    #Sad Path Tests
+    
+     def test200_101ShouldReturnError101(self):
+         parms={'cell':'r8c8','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'1883894'}
+         expectedResult = "{'status':'error 101: invalid input formatting'}"
+         actualResult = recommend._recommend(parms)
+                  
+         self.assertEqual(expectedResult, actualResult)
+     def test200_102ShouldReturnError102(self):
+         parms={'cell':'r1c2','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = "{'status:': 'error 102: cell already has number'}"
+         actualResult = recommend._recommend(parms)
+         self.assertEqual(expectedResult, actualResult)
+     def test200_103ShouldReturnError103(self):
+         parms={'cell':'r16c1','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = "{'status:': 'error 103: cell location out of bounds'}"
+         actualResult = recommend._recommend(parms)
+         self.assertEqual(expectedResult, actualResult)
+     def test200_104ShouldReturnError(self):
+         parms={'cell':'r0c1','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = "{'status:': 'error 103: cell location out of bounds'}"
+         actualResult = recommend._recommend(parms)
+         self.assertEqual(expectedResult, actualResult)
+     def test200_105ShouldReturnError103(self):
+         parms={'cell':'racl','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = "{'status':'error 101: invalid input formatting'}"
+         actualResult = recommend._recommend(parms)
+         self.assertEqual(expectedResult, actualResult)
+     def test200_106ShouldReturnError103(self):
+         parms={'cell':'r1c1','grid':'[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]' ,'integrity':'188c3894'}
+         expectedResult = "{'status':'error 101: invalid input formatting'}"
+         actualResult = recommend._recommend(parms)
+         self.assertEqual(expectedResult, actualResult)
+
